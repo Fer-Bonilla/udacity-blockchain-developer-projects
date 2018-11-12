da76312da4dd83bae14bd97d51be4a2045fc343d
Fernando Bonilla <33405407+Fer-Bonilla@users.noreply.github.com>
2018-11-12 10:25:54 -0500

    Index upodate with Rinkeby smartcontract reference
    Put index inside the smart_contracts folder

--- ethereum/smart_contracts/truffle-config.js
+++ ethereum/smart_contracts/truffle-config.js
@@ -11,6 +11,12 @@
  *     gasPrice: 10000000000,
  *   },
  */
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+//const mnemonic = 'flip affair sentence host suggest old clutch crucial jungle cube deal symbol';
+
+const mnemonic = 'forward gather transfer upper tongue file eagle benefit control excite advance estate';
+const infura = 'https://rinkeby.infura.io/v3/57ea1782cc6f4728bdd122fda6a31052'
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -20,7 +26,10 @@ module.exports = {
         host: '127.0.0.1',
         port: 8545,
         network_id: "*"
-      }
+      }, 
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic,infura),
+      network_id: 4,
     }
-
+  },
 };
--- ethereum/smart_contracts/truffle.js
+++ ethereum/smart_contracts/truffle.js
@@ -13,8 +13,10 @@
  */
 var HDWalletProvider = require('truffle-hdwallet-provider');
 
-const mnemonic = 'flip affair sentence host suggest old clutch crucial jungle cube deal symbol';
-const infura = 'https://rinkeby.infura.io/v3/a3b7da9fe90e4a938e6120bdc03792d8'
+
+//const mnemonic = 'flip affair sentence host suggest old clutch crucial jungle cube deal symbol';
+const mnemonic = 'forward gather transfer upper tongue file eagle benefit control excite advance estate';
+const infura = 'https://rinkeby.infura.io/v3/57ea1782cc6f4728bdd122fda6a31052'
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -24,6 +26,10 @@ module.exports = {
         host: '127.0.0.1',
         port: 8545,
         network_id: "*"
-      }   
+      }, 
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infura),
+      network_id: 4,
     }
+  },
 };
